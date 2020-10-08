4e7cb3c5cc10f955a4f74f30f49e7e425a03066f
Alie Stone <helloaliestone@gmail.com>
2020-10-08 12:40:02 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -9,12 +9,16 @@ attr_accessor :name, :artist, :files
     @@all << self
   end
 
-  def self.new_by_filename(files)
-    @artist_name = files.split(" - ")[0]
-    @song_name = files.split(" - ")[1]
+  def self.new_by_filename(file)
+    @artist_name = file.split(" - ")[0]
+    @song_name = file.split(" - ")[1]
     @song = Song.new(song_name)
     @song.artist_name = artist_name
     @song
+  end
+
+  def files
+    @files ||= Dir.entries(@path).select {|song| !File.directory?(song) && song.end_with?(".mp3")}
   end
 
   def self.all
